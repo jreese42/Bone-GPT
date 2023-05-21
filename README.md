f6e7b851896e73a62438a6c6b51263622cd9821e
jreese42 <jdreese42@gmail.com>
2023-05-21 18:26:56 -0400

    Add arg parsing

--- README.md
+++ README.md
@@ -3,3 +3,15 @@ The goal of this project is to build an AI voice pipeline for a halloween decora
 
 ## Building
 See the [Build guide](doc/Building.md) for information about requirements and how to build this project.
+
+## Running
+First, activate the venv with `source build/.venv/bin/activate`.
+Execute `build/boneGPT.sh`. Arguments are processed from the commandline first, then the config file, and finally the environment.
+
+|Argument|Description|Default|Config Parameter|Environment Variable|
+|--------|-----------|-------|----------------|--------------------|
+|`-c`,`--config`|path to .config file|`.config`|||
+|`--openai-key`|OpenAI API Key||`OpenAIApiKey`|`OPENAI_APIKEY`|
+|`--openai-organization`|OpenAI Organization||`OpenAIOrganization`|`OPENAI_ORGANIZATION`|
+
+I'll clean this up later, this is just how it works for now.
--- src/boneGPT.py
+++ src/boneGPT.py
@@ -1,15 +1,48 @@
 #!/usr/bin/env python3
 
+import os
+import sys
+import argparse
 import configparser
 import openai
 
+parser = argparse.ArgumentParser(
+                    prog='Bone-GPT', 
+                    description='AI Voice Assistant Pipeline for Halloween Decorations')
+parser.add_argument('-c', default='.config', dest='configFilePath',
+                    help='path to .config file')
+parser.add_argument('--openai-key', dest='openaiApiKey',
+                    help='An API Key for OpenAI. May alternatively be provided in the .config file or in the environment as OPENAI_APIKEY')
+parser.add_argument('--openai-organization', dest='openaiOrganization',
+                    help='An Organization for the OpenAI API. If omitted, the default Organization for your OpenAI account will be used. May alternatively be provided in the .config file or in the environment as OPENAI_ORGANIZATION')
+
+args = parser.parse_args()
+
 config = configparser.ConfigParser()
-config.read('build/.config') #TODO: Take as input
+config.read(args.configFilePath)
 
+# Resolve OpenAI Arguments from config or env if not provided as args
+if not args.openaiApiKey:
+    args.openaiApiKey = config['OpenAI']['OpenAIApiKey']
+if not args.openaiOrganization:
+    args.openaiOrganization = config['OpenAI']['OpenAIOrganization']
 
-#TODO: Validate key exists
-openai.api_key = config['OpenAI']['OpenAIApiKey']; #TODO: Or from params, or from env
-openai.organization = config['OpenAI']['OpenAIOrganization'];
+if not args.openaiApiKey:
+    args.openaiApiKey = os.getenv('OPENAI_APIKEY')
+if not args.openaiOrganization:
+    args.openaiOrganization = os.getenv('OPENAI_ORGANIZATION')
+
+if not args.openaiApiKey:
+    print('\n\nError: Unable to Resolve OpenAI API Key\n\n', file=sys.stderr)
+    parser.print_help()
+    exit(-1)
+
+openai.api_key = args.openaiApiKey
+openai.organization = args.openaiOrganization
 
 completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Who are you?"}])
 print(completion.choices[0].message.content)
+
+#For now, dump to the TTS pipeline as a system call
+voice_cmd = "echo '{}' | ./piper --model en-us-ryan-medium.onnx --output_raw | ffplay -autoexit -nodisp -f s16le -ar 22050 -i -"
+os.system(voice_cmd.format(completion.choices[0].message.content))
